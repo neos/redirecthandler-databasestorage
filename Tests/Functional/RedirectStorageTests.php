51a168398a0e0fd793b0fd77e22331a6c3416098
Sebastian Helzle <sebastian@helzle.it>
2020-05-11 12:17:18 +0200

    FEATURE: Update existing redirects with absolute targets
    Without this change redirects that have
    target uris with absolute paths were not
    updated when a new redirect is added
    that matches both domain and source path.
    
    Now they are updated when host + sourcepath
    match exactly to their target.
    Also the target remains absolute and only
    the path is updated.

---
+++ Tests/Functional/RedirectStorageTests.php
@@ -0,0 +1,87 @@
+<?php
+namespace Neos\RedirectHandler\DatabaseStorage\Tests\Functional;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Tests\FunctionalTestCase;
+use Neos\RedirectHandler\DatabaseStorage\Domain\Repository\RedirectRepository;
+use Neos\RedirectHandler\Storage\RedirectStorageInterface;
+
+/**
+ * Functional tests for the RedirectService and dependant classes
+ */
+class RedirectStorageTests extends FunctionalTestCase
+{
+    /**
+     * @var boolean
+     */
+    protected static $testablePersistenceEnabled = true;
+
+    /**
+     * @var RedirectStorageInterface
+     */
+    protected $redirectStorage;
+
+    /**
+     * @var RedirectRepository
+     */
+    protected $redirectRepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->redirectStorage = $this->objectManager->get(RedirectStorageInterface::class);
+        $this->redirectRepository = $this->objectManager->get(RedirectRepository::class);
+    }
+
+    /**
+     * @test
+     */
+    public function addRedirectResolvesRedirectChainsWithRelativeTargetUris()
+    {
+        $sourcePath = 'some/old/product';
+        $oldTargetUri = 'productB';
+        $newTargetUri = 'product/B';
+
+        $this->redirectStorage->addRedirect($sourcePath, $oldTargetUri);
+        $this->redirectRepository->persistEntities();
+
+        $this->redirectStorage->addRedirect($oldTargetUri, $newTargetUri);
+
+        $relativeRedirect = $this->redirectStorage->getOneBySourceUriPathAndHost($sourcePath);
+        $this->assertSame($newTargetUri, $relativeRedirect->getTargetUriPath());
+    }
+
+    /**
+     * @test
+     */
+    public function addRedirectResolvesRedirectChainsWithAbsoluteTargetUris()
+    {
+        $sourcePath = 'some/old/product';
+        $oldTargetUri = 'https://www.example.org/productA';
+        $newTargetUri = 'product/A';
+        $newAbsoluteTargetUri = 'https://www.example.org/' . $newTargetUri;
+
+        $this->redirectStorage->addRedirect($sourcePath, $oldTargetUri);
+        $this->redirectRepository->persistEntities();
+
+        $this->redirectStorage->addRedirect('productA', $newTargetUri);
+
+        $absoluteRedirect = $this->redirectStorage->getOneBySourceUriPathAndHost($sourcePath);
+
+        $this->assertSame($oldTargetUri, $absoluteRedirect->getTargetUriPath());
+
+        $this->redirectStorage->addRedirect('productA', $newTargetUri, null, ['www.example.org']);
+
+        $absoluteRedirect = $this->redirectStorage->getOneBySourceUriPathAndHost($sourcePath);
+        $this->assertSame($newAbsoluteTargetUri, $absoluteRedirect->getTargetUriPath());
+    }
+}
